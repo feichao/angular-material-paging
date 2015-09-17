05a01230eb91f5a8bb53c557d87ba157a1f0ed09
FeichaoWan <len.may@foxmail.com>
2015-09-17 19:29:09 +0800

    bugfix

--- src/angular-material-paging.js
+++ src/angular-material-paging.js
@@ -23,11 +23,13 @@
       template: [
         '<div layout="row" class="wan-material-paging" layout-align="{{ position }}">',
         '<md-button class="md-raised md-primary wmp-button" ng-click="vm.gotoFirst()">{{ vm.first }}</md-button>',
+        '<md-button class="md-raised wmp-button" ng-click="vm.getoPre()" ng-show="vm.index - 1 >= 0">...</md-button>',
         '<md-button class="md-raised wmp-button" ng-repeat="i in vm.stepInfo"',
         ' ng-click="vm.goto(vm.index + i)" ng-show="vm.page[vm.index + i]" ',
         ' ng-class="{true: \'md-primary\', false: \'\'}[vm.page[vm.index + i] === currentPage]">',
         ' {{ vm.page[vm.index + i] }}',
         '</md-button>',
+        '<md-button class="md-raised wmp-button" ng-click="vm.getoNext()" ng-show="vm.index + vm.step < wmpTotal">...</md-button>',
         '<md-button class="md-raised md-primary wmp-button" ng-click="vm.gotoLast()">{{ vm.last }}</md-button>',
         '</div>'
       ].join('')
@@ -46,16 +48,17 @@
     vm.step = $scope.step;
 
     vm.goto = function(index) {
-      var page = vm.page[index];
-      if (page === '...' && index === vm.index) {
-        $scope.currentPage = vm.page[index - 1];
-        vm.index -= vm.step;
-      } else if (page === '...' && index === vm.index + vm.step) {
-        $scope.currentPage = vm.page[index + 1];
-        vm.index += vm.step;
-      } else {
-        $scope.currentPage = page;
-      }
+      $scope.currentPage = vm.page[index];
+    };
+
+    vm.getoPre = function(){
+      $scope.currentPage = vm.index;
+      vm.index -= vm.step;
+    };
+
+    vm.getoNext = function(){
+      vm.index += vm.step;
+      $scope.currentPage = vm.index + 1;
     };
 
     vm.gotoFirst = function(){
@@ -64,9 +67,9 @@
     };
 
     vm.gotoLast = function(){
-      var index = parseInt(vm.realyTotal / vm.step) * vm.step;
+      vm.index = parseInt($scope.wmpTotal / vm.step) * vm.step;
+      vm.index === $scope.wmpTotal ? vm.index = vm.index - vm.step : '';
       $scope.currentPage = $scope.wmpTotal;
-      index !== vm.realyTotal ? vm.index = index : null;
     };
 
     $scope.$watch('currentPage', function() {
@@ -78,27 +81,22 @@
     });
 
     vm.init = function() {
-      vm.realyTotal = $scope.wmpTotal;
       vm.stepInfo = (function() {
         var i, result = [];
-        for (i = 0; i <= vm.step; i++) {
+        for (i = 0; i < vm.step; i++) {
           result.push(i)
         }
         return result;
       })();
+
       vm.page = (function() {
-        var i = 0, result = [];
-        for (i = 0; i < $scope.wmpTotal; i++) {
-          if (i === vm.step || i % (vm.step - 1) === 1 && i > vm.step) {
-            if ($scope.wmpTotal - i !== 1) {
-              result.push('...');
-              vm.realyTotal++;
-            }
-          }
-          result.push(i + 1);
+        var i, result = [];
+        for (i = 1; i <= $scope.wmpTotal; i++) {
+          result.push(i);
         }
         return result;
       })();
+
     };
   }
 
