92b616913cc99519d34f1d6a4aca1d56fe0b6e6b
Austin Buddington <austin.j.buddington@gmail.com>
2016-07-14 11:33:59 -0400

    updates to css classes

--- src/angular-material-paging.js
+++ src/angular-material-paging.js
@@ -21,24 +21,21 @@
       controller: Controller,
       controllerAs: 'vm',
       template: [
-        '<div layout="row" class="wan-material-paging" layout-align="{{ position }}">',
-        '<md-button class="md-raised md-primary wmp-button" aria-label="First" ng-click="vm.gotoFirst()">{{ vm.first }}</md-button>',
-        '<md-button class="md-raised wmp-button" aria-label="Previous" ng-click="vm.getoPre()" ng-show="vm.index - 1 >= 0">...</md-button>',
-        '<md-button class="md-raised wmp-button" aria-label="Go to page {{i+1}}" ng-repeat="i in vm.stepInfo"',
+        '<div layout="row" class="paging" layout-align="{{ position }}">',
+        '<a class="paging__link paging__link--first" aria-label="First" ng-click="vm.gotoFirst()">{{ vm.first }}</a>',
+        '<a class="paging__link paging__link--previous" aria-label="Previous" ng-click="vm.getoPre()" ng-show="vm.index - 1 >= 0">...</a>',
+        '<a class="paging__link" aria-label="Go to page {{i+1}}" ng-repeat="i in vm.stepInfo"',
         ' ng-click="vm.goto(vm.index + i)" ng-show="vm.page[vm.index + i]" ',
         ' ng-class="{true: \'md-primary\', false: \'\'}[vm.page[vm.index + i] === currentPage]">',
         ' {{ vm.page[vm.index + i] }}',
-        '</md-button>',
-        '<md-button class="md-raised wmp-button" aria-label="Next" ng-click="vm.getoNext()" ng-show="vm.index + vm.step < wmpTotal">...</md-button>',
-        '<md-button class="md-raised md-primary wmp-button" aria-label="Last" ng-click="vm.gotoLast()">{{ vm.last }}</md-button>',
+        '</a>',
+        '<a class="paging__link paging__link--next" aria-label="Next" ng-click="vm.getoNext()" ng-show="vm.index + vm.step < wmpTotal">...</a>',
+        '<a class="paging__link paging__link--last" aria-label="Last" ng-click="vm.gotoLast()">{{ vm.last }}</a>',
         '</div>'
       ].join('')
     };
   }
 
-  /**
-   * @ngInject
-   */
   var	Controller = ['$scope', function ($scope) {
     var vm = this;
 
@@ -51,22 +48,22 @@
       $scope.currentPage = vm.page[index];
     };
 
-    vm.getoPre = function(){
+    vm.getoPre = function() {
       $scope.currentPage = vm.index;
       vm.index -= vm.step;
     };
 
-    vm.getoNext = function(){
+    vm.getoNext = function() {
       vm.index += vm.step;
       $scope.currentPage = vm.index + 1;
     };
 
-    vm.gotoFirst = function(){
+    vm.gotoFirst = function() {
       vm.index = 0;
       $scope.currentPage = 1;
     };
 
-    vm.gotoLast = function(){
+    vm.gotoLast = function() {
       vm.index = parseInt($scope.wmpTotal / vm.step) * vm.step;
       vm.index === $scope.wmpTotal ? vm.index = vm.index - vm.step : '';
       $scope.currentPage = $scope.wmpTotal;
@@ -96,8 +93,6 @@
         }
         return result;
       })();
-
     };
   }];
-
 })();
